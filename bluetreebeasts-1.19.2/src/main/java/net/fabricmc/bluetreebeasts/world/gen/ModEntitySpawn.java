fa1c0461572db83be537e541e38c97615b31f5eb
Blue-Tree-Thing <zantcoop3@gmail.com>
2023-08-21 12:10:21 -0400

    added various updates and ocean flish

--- bluetreebeasts-1.19.2/src/main/java/net/fabricmc/bluetreebeasts/world/gen/ModEntitySpawn.java
+++ bluetreebeasts-1.19.2/src/main/java/net/fabricmc/bluetreebeasts/world/gen/ModEntitySpawn.java
@@ -14,9 +14,11 @@ public class ModEntitySpawn {
 
     public static void addEntitySpawn(){
         BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.JUNGLE, BiomeKeys.SPARSE_JUNGLE), SpawnGroup.CREATURE, ModEntities.FORESTFLISH, 50, 5,10);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.OCEAN, BiomeKeys.BEACH), SpawnGroup.CREATURE, ModEntities.OCEANFLISH, 40, 10,12);
 
 
         SpawnRestriction.register(ModEntities.FORESTFLISH, SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnimalEntity::isValidNaturalSpawn);
+        SpawnRestriction.register(ModEntities.OCEANFLISH, SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnimalEntity::isValidNaturalSpawn);
     }
 
 
